4c1460629c61190afaf60e6fa6feba4acd948e28
unknown <kaludivine545@gmail.com>
2023-03-05 09:40:30 +0530

    updated home page

--- audiophoile/src/components/shop/Shop.js
+++ audiophoile/src/components/shop/Shop.js
@@ -10,13 +10,14 @@ const Shop = () => {
           const { id, img, url, name } = item
           return (
             <div
-              className='relative flex flex-col w-[300px] h-[150px] space-y-4  bg-grayColor justify-center items-center rounded-md'
+              className='relative flex flex-col w-[300px] h-[180px] space-y-4  bg-grayColor justify-center items-center rounded-md'
               key={id}
             >
               <div className='mx-auto mt-[-50px] bg-transparent '>
                 <img className='w-20' src={img} alt='' />
+                <div className='bg-black h-[8px] rounded-full w-20 blur-md'></div>
               </div>
-              <div className='shadow-sm'></div>
+
               <p className='uppercase font-semibold'>{name}</p>
               <Link className='flex items-center gap-1 uppercase' to={url}>
                 shop <FaChevronRight className='font-thin' />
